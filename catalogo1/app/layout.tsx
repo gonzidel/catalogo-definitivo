492c2f682c0b83ae36f252c3cd544d7717f35569
gonzidel <gonzidel@gmail.com>
2026-06-14 17:22:10 -0300

    feat: agregar Google Analytics, Meta Pixel y Microsoft Clarity

--- catalogo1/app/layout.tsx
+++ catalogo1/app/layout.tsx
@@ -60,6 +60,34 @@ export default function RootLayout({
           href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,400;0,600&display=swap"
         />
         <meta name="theme-color" content="#CD844D" />
+
+        {/* Google Analytics */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-2JDYZW1KD6" />
+        <script dangerouslySetInnerHTML={{ __html: `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-2JDYZW1KD6', { send_page_view: true });
+        `}} />
+
+        {/* Meta Pixel */}
+        <script dangerouslySetInnerHTML={{ __html: `
+          !function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+          n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;
+          n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;
+          t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}
+          (window,document,'script','https://connect.facebook.net/en_US/fbevents.js');
+          fbq('init','988002930324230');
+          fbq('track','PageView');
+        `}} />
+
+        {/* Microsoft Clarity */}
+        <script dangerouslySetInnerHTML={{ __html: `
+          (function(c,l,a,r,i,t,y){c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+          t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+          y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+          })(window,document,"clarity","script","w7h6cytm9j");
+        `}} />
       </head>
       <body>
         {/* Header needs Suspense because SearchBar uses useSearchParams */}
